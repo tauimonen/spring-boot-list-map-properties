b1953975ca09da492fac615bd1bce4db59472f14
vgtoui <toni.uimonen@tuni.fi>
2024-02-26 11:15:31 +0200

    Create BookmarkProperties and Website records

--- src/main/java/dev/tau/listmapprops/ListMapPropsApplication.java
+++ src/main/java/dev/tau/listmapprops/ListMapPropsApplication.java
@@ -1,11 +1,12 @@
 package dev.tau.listmapprops;
 
 import dev.tau.listmapprops.list.AccountProperties;
+import dev.tau.listmapprops.map.BookmarkProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(AccountProperties.class)
+@EnableConfigurationProperties({AccountProperties.class, BookmarkProperties.class})
 @SpringBootApplication
 public class ListMapPropsApplication {
 
---
+++ src/main/java/dev/tau/listmapprops/map/Website.java
@@ -0,0 +1,4 @@
+package dev.tau.listmapprops.map;
+
+public record Website(String url) {
+}
